338d28f1aa22835ccd95e3d1ae936573aa7b9030
Sebastian Villalobos <dsvillacorta@gmail.com>
2024-09-08 22:11:21 -0600

    DS CloudSwift | Version 2.0.2 - RESTful Web Service

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With");
 
-echo json_encode("DS CloudSwift | Version 2.0.1 - RESTful Web Service");
+echo json_encode("DS CloudSwift | Version 2.0.2 - RESTful Web Service");
